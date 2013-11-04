d00259a4d8d5113005b2e4e6fba28cdaef4981d5
Philip Urban <urban.philip@googlemail.com>
2013-11-04 13:54:16 +0100

    refactored to depend on rctravel station interface

--- src/main/java/de/raidcraft/rctravel/api/station/AbstractStation.java
+++ src/main/java/de/raidcraft/rctravel/api/station/AbstractStation.java
@@ -27,4 +27,22 @@ public abstract class AbstractStation implements Station {
 
         return location;
     }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractStation that = (AbstractStation) o;
+
+        return name.equals(that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+
+        return name.hashCode();
+    }
 }
