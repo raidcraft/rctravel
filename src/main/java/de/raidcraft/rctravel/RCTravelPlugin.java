6d8c500e5774a91040a0d9fcc0d088b92d8bda9b
Philip Urban <urban.philip@googlemail.com>
2013-11-04 12:41:16 +0100

    initial commit

---
+++ src/main/java/de/raidcraft/rctravel/RCTravelPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.rctravel;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Philip Urban
+ */
+public class RCTravelPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
