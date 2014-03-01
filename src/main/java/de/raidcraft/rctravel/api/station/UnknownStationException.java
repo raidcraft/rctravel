1b338803ac36977997369d8ba07d9e711269a91c
Silthus <silthus@raid-craft.de>
2014-03-01 17:59:40 +0100

    added unknown station exception

---
+++ src/main/java/de/raidcraft/rctravel/api/station/UnknownStationException.java
@@ -0,0 +1,14 @@
+package de.raidcraft.rctravel.api.station;
+
+import de.raidcraft.api.RaidCraftException;
+
+/**
+ * @author Silthus
+ */
+public class UnknownStationException extends RaidCraftException {
+
+    public UnknownStationException(String message) {
+
+        super(message);
+    }
+}
