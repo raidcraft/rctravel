45ca1bc36b67b7b69aeba9ba8bd9ecfebb825bf2
mdoering <mdoering@interhyp.de>
2014-06-24 13:25:35 +0200

    added isFree method

--- src/main/java/de/raidcraft/rctravel/api/station/Chargeable.java
+++ src/main/java/de/raidcraft/rctravel/api/station/Chargeable.java
@@ -5,6 +5,11 @@ package de.raidcraft.rctravel.api.station;
  */
 public interface Chargeable {
 
+    public default boolean isFree() {
+
+        return getPrice() <= 0;
+    }
+
     public double getPrice();
 
     public double getPrice(int distance);
