db33360f7d6f87e97b4377d4e9c969bb2094ded4
Michael Reichenbach <michael@mirenoba.com>
2018-09-28 19:21:02 +0200

    start rollback to java 8

---
+++ src/test/java/de/raidcraft/GenerateDbMigration.java
@@ -0,0 +1,33 @@
+package de.raidcraft;
+
+import io.ebean.annotation.Platform;
+import io.ebean.dbmigration.DbMigration;
+import io.ebeaninternal.dbmigration.DefaultDbMigration;
+
+import java.io.IOException;
+
+public class GenerateDbMigration {
+
+    /**
+     * Generate the next "DB schema DIFF" migration.
+     * <p>
+     * These migration are typically run using FlywayDB, Liquibase
+     * or Ebean's own built in migration runner.
+     * </p>
+     */
+    public static void main(String[] args) throws IOException {
+
+        // optionally specify the version and name
+        System.setProperty("ddl.migration.version", "1.0");
+        System.setProperty("ddl.migration.name", "initial");
+
+        // generate a migration using drops from a prior version
+        //System.setProperty("ddl.migration.pendingDropsFor", "1.2");
+
+        DbMigration dbMigration = new DefaultDbMigration();
+        dbMigration.setPlatform(Platform.MYSQL);
+        // generate the migration ddl and xml
+        // ... with EbeanServer in "offline" mode
+        dbMigration.generateMigration();
+    }
+}
