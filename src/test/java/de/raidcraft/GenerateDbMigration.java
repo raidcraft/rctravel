3f0291d4c8bc044958483d383be2a110b3c7d9e4
Michael Reichenbach <michael@mirenoba.com>
2018-09-29 11:49:13 +0200

    feat(db): create initial migrations

--- src/test/java/de/raidcraft/GenerateDbMigration.java
+++ src/test/java/de/raidcraft/GenerateDbMigration.java
@@ -17,15 +17,10 @@ public class GenerateDbMigration {
      */
     public static void main(String[] args) throws IOException {
 
-        // generate a migration using drops from a prior version
-        //System.setProperty("ddl.migration.pendingDropsFor", "1.2");
-
         DbMigration dbMigration = new DefaultDbMigration();
         dbMigration.setPlatform(Platform.MYSQL);
-        dbMigration.setName("initial");
-        dbMigration.setVersion("1.0");
         // generate the migration ddl and xml
         // ... with EbeanServer in "offline" mode
-        String script = dbMigration.generateMigration();
+        dbMigration.generateMigration();
     }
 }
